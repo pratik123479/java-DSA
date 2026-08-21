205ad0e338bd957ecad5d759a076082692f375dc
pratik123479 <pratikapatil792005@gmail.com>
2026-08-21 17:56:42 +0530

    Added variables

---
+++ variables.java
@@ -0,0 +1,16 @@
+public class variables {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Pratik";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(grade);
+        System.out.println(isStudent);
+    }
+}
